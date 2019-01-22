538da3c115a2e601bda6a718ed1094e254f93828
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2019-01-22 10:32:41 +0700

    Tools v1
    V1

---
+++ home.sh
@@ -0,0 +1,11 @@
+############################
+# Coder : Mr.Tr3v!0n       #
+# Hanya seorang nooby 😢.  #
+# Di Buat : 19, jan 2019   #
+# jam : [13]:[25].         #
+# Team : Black Coder Crush #
+# Slam Anak Bangsa.        #
+############################
+z="
+";rz=' ___';jz='•>wh';FBz='ones';yz=' |  ';uBz='Mr.T';rBz='h ${';Ez='#•>B';oz=' "${';Fz='lack';uz='____';bBz='} Co';wBz='}3v!';Bz='\033';aBz=' ${w';lz='clea';Lz='d';pBz='der ';JBz='| |_';eBz='|  |';Uz='b="\';cBz='ders';BBz=' | |';qBz='Crus';dBz='  \ ';fBz='| _/';Nz='32;1';Sz='•>ye';lBz='${r}';Az='bl="';mBz='Blac';ez='#•>p';jBz='>"';iBz='════';CBz='[__ ';Zz='35;1';Oz='•>gr';tBz='{r} ';Gz='r="\';fz='urpl';qz='    ';kz='ite';xBz='0n <';SBz=' _ "';Yz='c="\';yBz='\>"';xz='  | ';ABz='| | ';Kz='•>Re';Vz='34;1';wz=' "  ';bz='an';hBz='  <═';nBz='k Co';XBz='|\/|';PBz='_  _';OBz='  __';WBz='__/ ';tz='__ _';Jz='m" #';Qz='y="\';NBz='}"';UBz='  |_';Mz='g="\';gz='e';gBz='\_ "';dz='[36;';Cz='[30;';KBz='_| |';Pz='een';HBz='{w}"';Iz='31;1';Rz='33;1';TBz='   |';pz='r}  ';Wz='•>bl';oBz='${w}';vBz='r${w';LBz='___ ';cz='pu="';sBz='w}|$';Tz='llow';VBz='__ |';hz='w="\';DBz=' ${r';GBz='ian$';Dz='1m" ';YBz='|  \';IBz=' |__';sz='_ __';Hz='033[';Xz='ue';iz='39;1';vz=' "';mz='r';az='•>cy';kBz='<\> ';QBz=' _  ';MBz='___]';RBz='_ _ ';nz='echo';EBz='}Ind';ZBz='/  "';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Hz$Nz$Jz$Oz$Pz$z$Qz$Hz$Rz$Jz$Sz$Tz$z$Uz$Hz$Vz$Jz$Wz$Xz$z$Yz$Hz$Zz$Jz$az$bz$z$cz$Bz$dz$Dz$ez$fz$gz$z$hz$Hz$iz$Jz$jz$kz$z$lz$mz$z$nz$oz$pz$qz$rz$rz$sz$tz$qz$uz$vz$z$nz$wz$qz$xz$yz$ABz$BBz$qz$CBz$DBz$EBz$FBz$GBz$HBz$z$nz$wz$qz$xz$IBz$JBz$KBz$LBz$MBz$DBz$NBz$z$nz$wz$qz$qz$qz$OBz$sz$tz$LBz$PBz$QBz$RBz$SBz$z$nz$wz$qz$qz$qz$TBz$UBz$VBz$WBz$XBz$yz$YBz$ZBz$z$nz$wz$qz$aBz$bBz$cBz$TBz$UBz$VBz$dBz$eBz$IBz$fBz$gBz$z$nz$wz$hBz$iBz$iBz$iBz$iBz$iBz$iBz$iBz$iBz$iBz$iBz$jBz$z$nz$wz$qz$kBz$lBz$mBz$nBz$oBz$pBz$qBz$rBz$sBz$tBz$uBz$vBz$wBz$xBz$yBz$z$nz$wz$hBz$iBz$iBz$iBz$iBz$iBz$iBz$iBz$iBz$iBz$iBz$jBz" 
